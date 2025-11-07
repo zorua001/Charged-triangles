4ffc3a75d2c56f46516398a453b9d4cb87d93f3b
Filip Eriksson <filipsola1@gmail.com>
2025-11-07 13:28:11 +0100

    ord

--- Homogeneous.py
+++ Homogeneous.py
@@ -19,23 +19,28 @@ def stora_q (a,b,c,d,e,n):
 
 def stora_j (a,a_prim,b,b_prim,c,c_prim,d,e,n):
     k = a*d-b*c
-    k_prim = a_prim*b_prim-b_prim*c_prim
+    k_prim = a_prim*d-b_prim*c ## Försöker använda formeln för k från linär 
     rad_1 = np.log((a+b*n + np.sqrt((a+b*n)**2+(c+d*n)**2+e**2))/(a_prim+b_prim*n+np.sqrt((a_prim+b_prim*n)**2+(c+d*n)**2+e**2)))*(c+d*n)/d 
     rad_2_1 = k*stora_p(a, b, c, d, e, n)/(2*d*np.sqrt(b**2+d**2))-k_prim*stora_p(a_prim, b_prim, c, d, e, n)/(2*d*np.sqrt(b_prim**2+d**2))
     rad_2_2 = (stora_q(a, b, c, d, e, n)-stora_q(a_prim, b_prim, c, d, e, n))*e/d
     return rad_1+rad_2_1+rad_2_2
 
 def area (triange):
-    return sum(abs(np.cross(triange[1]-triange[0],triange[2]-triange[0])))/2
+    temp = abs(np.cross(triange[1]-triange[0],triange[2]-triange[0]))
+    m = 0
+    for i in temp:
+        m = m+i**2
+    return m**0.5/2
     
     
 def homogeneous (triange,point):
-    alpha = sum([abs(triange[0][k]-triange[2][k])for k in range (3)])
+    alpha = sum([(triange[0][k]-triange[2][k])**2 for k in range (3)])**0.5
     b_prim = np.dot((triange[0]-triange[2]),np.transpose(triange[1]-triange[2]))/(alpha**2)
     a_prim = np.dot((triange[0]-triange[2]),np.transpose(triange[2]-point))/(alpha**2)
     a = 1 + a_prim
     b = b_prim -1
-    q = sum(abs(np.cross(triange[0]-triange[2],triange[1]-triange[2])))
+    q_t = abs(np.cross(triange[0]-triange[2],triange[1]-triange[2]))
+    q = sum([q_t[k]**2 for k in range (3)])**0.5
     n = np.cross(triange[0]-triange[2],triange[1]-triange[2])/q
     c = np.dot(n, np.transpose(np.cross(triange[0]-triange[2],triange[2]-point)))/(alpha**2)
     d = q/(alpha**2)
@@ -45,6 +50,8 @@ def homogeneous (triange,point):
     return (2*area(triange)/alpha)*(stora_j(a, a_prim, b, b_prim, c, c_prim, d, e, 1)-stora_j(a, a_prim, b, b_prim, c, c_prim, d, e, 0))
 
 
-n = homogeneous(np.array([[0,0,0],[1,0,0],[0.25,1,0]]), np.array([2,2,2]))
+n = homogeneous(np.array([[1,0,0],[0.5,0.867,0],[0,0,0]]), np.array([0.5,0.289,0.5]))
+m = homogeneous(np.array([[0,0,0],[1,0,0], [0.25,1,0]]), np.array([2,0,2]))
 
 print(n)
+#print(m)
