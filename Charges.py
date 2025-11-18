798539a3115c551ab9dbf1f6d72edaa86d0a68dd
Filip Eriksson <filipsola1@gmail.com>
2025-11-18 11:19:00 +0100

    m

--- Charges.py
+++ Charges.py
@@ -7,17 +7,21 @@ Created on Wed Nov  5 13:35:25 2025
 
 import numpy as np
 
+def beräkning (charge_coordinate,point):
+    korta =sum([(point[k]-charge_coordinate[k])**2 for k in range (3)])**0.5
+    if korta != 0:
+        return 1/korta
+    return korta
+
 def charge(charge_coordinates,points,potentia):
     print("hello")
-    distance = np.array
+    distance = np.zeros([len(charge_coordinates),len(points)])
     for i in range(len(points)):
         for j in range(len(points)):
             korta =sum([(points[i][k]-charge_coordinates[j][k])**2 for k in range (3)])**0.5
             if korta != 0:
                 korta = 1/korta
-            distance = np.append(distance,korta) 
-    distance = np.delete(distance, 0)
-    distance = distance.reshape(len(points), len(points))
+            distance[i][j] = korta 
     potentia = np.ones(len(points))*potentia
     print("hej")
     return np.linalg.lstsq(distance.astype('float') , potentia.astype('float'),rcond=-1)[0]
