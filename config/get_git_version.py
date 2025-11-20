0ee5e908c256fa9225a0f9355149f1d36bcbd687
Hampus Berndt <hampusberndt@gmail.com>
2025-11-20 12:32:16 +0100

    Forgot to save changes

--- config/get_git_version.py
+++ config/get_git_version.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Nov 20 12:07:45 2025
+Gets the current git version of the code.
 
 @author: Hampus Berndt
 """
+import git
 
+def get_git_version():
+    try:
+        # Assuming the current directory is part of a Git repository
+        repo = git.Repo(search_parent_directories=True)
+        return repo.head.object.hexsha  # Get the commit hash
+    except Exception as e:
+        print(f"Error retrieving Git version: {e}")
+        return "unknown"
